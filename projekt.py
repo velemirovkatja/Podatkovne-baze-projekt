80e5e00513e2c9abe0a6d24ed7095f7f651a7b82
Katja Velemirov <katja.velemirov@student.fmf.uni-lj.si>
2014-12-15 11:47:01 +0100

    dodane funkcije in spremenjene tabele

--- projekt.py
+++ projekt.py
@@ -5,16 +5,23 @@ datoteka_baze = "kinospored.sqlite3"
 # iskanje filmov po naslovu
 # poišče tudi, če ne poznamo celega naslova, vendar samo ključne besede (v tem primeru nam vrne vse filme s to ključno besedo)
 def poisciFilme(beseda):
-    c = baza.cursor()
-    c.execute("""SELECT id, ime FROM Seznam filmov WHERE ime LIKE %?%""", [beseda])
+    with baza:
+        baza.execute("""SELECT id, ime FROM Seznam filmov WHERE ime LIKE %?%""", [beseda])
     filmi = c.fetchall()
-    c.close()
     
     return filmi
 
-# funkcija, ki prodaja vstopnice
-def prodajanjeVstopnic():
-    c = baza.cursor()
-    #c.execute("""SELECT"""
+# projekcija za določen film
+def projekcjaKateregaFilma(): # id v parametru???? kaj mi vrne???
+    with baza:
+        baza.execute("""INSERT INTO projekcija (film) SELECT id FROM Seznam filmov""")
+        
+
+# vstopnica za določen film
+def vstopnicaKatereProjekcije():
+    with baza:
+        baza.execute("""INSERT INTO vstopnica (projekcija) SELECT id FROM projekcija""")
+        
+
 
 baza = sqlite3.connect(datoteka_baze, isolation_level = None)
